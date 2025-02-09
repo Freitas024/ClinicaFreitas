67b02a829fa82f72afab0812826f0bf7852f42f4
Freitas024 <viniciusfreitassilva1999@gmail.com>
2025-02-09 14:17:58 -0300

    fix: reajustando codigo para implementação no vercel.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./pages/Principal";
+import Principal from "./pages/Principal";
 import Paciente from "./pages/CadastroPaciente";
 import Profissional from "./pages/CadastroProfissional";
 import Consultas from "./pages/Consultas";
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Principal />}/>
         <Route path="/CadastroPaciente" element={<Paciente />}/>
         <Route path="/CadastroProfissional" element={<Profissional />}/>
         <Route path="/Consultas" element={<Consultas />}/>
